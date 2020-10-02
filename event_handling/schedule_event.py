bb5c94ecebc3c250c05051270ffb7d57305d44dd
nikhilcbhat <bhatnikhil314@gmail.com>
2020-10-02 13:42:33 -0400

    Resolved id generation

--- event_handling/schedule_event.py
+++ event_handling/schedule_event.py
@@ -1,6 +1,7 @@
 import argparse
 import boto3
 import time
+import datetime
 
 allowed_event_types = {
     "WATER", "AERATE"
@@ -9,15 +10,19 @@ allowed_event_types = {
 def parse_date(date):
     return int(time.mktime(time.strptime(date, "%m/%d/%Y %H:%M"))*1000)
 
+def generate_event_id():
+    now = datetime.datetime.utcnow()
+    return int((now - datetime.datetime(1970, 1, 1)).total_seconds())
+
 def schedule_event(event_type, date, frequency):
     if event_type not in allowed_event_types:
         print(f"Invalid event type: {event_type} must be one of: {allowed_event_types}")
 
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Events')
-    item_dict = {"EventID": table.item_count, "EventType": event_type, "Date": parse_date(date)}
+    item_dict = {"EventID": generate_event_id(), "EventType": event_type, "Date": parse_date(date)}
     if frequency is not None:
-        item_dict["Frequency"] = frequency
+        item_dict["Frequency"] = int(frequency)
 
     table.put_item(Item=item_dict)
 
