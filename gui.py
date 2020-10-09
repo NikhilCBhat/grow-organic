1b833f117105cf77c562549a261db3a0247f6a6b
nikhilcbhat <bhatnikhil314@gmail.com>
2020-10-09 17:47:17 -0400

    Added title block

--- gui.py
+++ gui.py
@@ -9,32 +9,34 @@ class GrowOrganicGUI(tk.Tk):
         s = ttk.Style(self)
         s.theme_use('clam')
 
+        ttk.Label(self, text='Grow Organic!\n', font="Arial 20").grid(row=0, columnspan=2)
         self._add_calendar_widget()
         self._add_event_time_widget()
         self._add_event_type_widget()
-        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=3, columnspan=2)
+        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=4, columnspan=2)
 
     def _add_calendar_widget(self):
         self.calendar_date = None
         self.calendar_widget = Calendar(self,font="Arial 14", 
             selectmode='day',cursor="hand1", year=2020, month=10, day=5)
-        self.calendar_widget.grid(row=0, columnspan=2)
+        self.calendar_widget.grid(row=1, columnspan=2)
 
     def _add_event_time_widget(self):
-        ttk.Label(self, text='Select Time:').grid(row=1, column=0)
+        ttk.Label(self, text='Select Time:').grid(row=2, column=0)
         self.event_time = tk.StringVar(self)
         self.event_time.set("0:00")
-        tk.OptionMenu(self, self.event_time, *[f"{time}:00" for time in range(24)]).grid(row=1, column=1)
+        tk.OptionMenu(self, self.event_time, *[f"{time}:00" for time in range(24)]).grid(row=2, column=1)
 
     def _add_event_type_widget(self):
-        ttk.Label(self, text='Event Type:').grid(row=2, column=0)
+        ttk.Label(self, text='Event Type:').grid(row=3, column=0)
         self.event_type = tk.StringVar(self)
         self.event_type.set("AERATE")
-        tk.OptionMenu(self, self.event_type, *["AERATE", "WATER"]).grid(row=2, column=1)
+        tk.OptionMenu(self, self.event_type, *["AERATE", "WATER"]).grid(row=3, column=1)
 
     def scheduleEvent(self):
-        print(f"Scheduling {self.event_type.get()} event on \
-        {self.calendar_widget.selection_get()} at {self.event_time.get()}")
+        print(
+            f"Scheduling {self.event_type.get()} event on " +
+            f"{self.calendar_widget.selection_get()} at {self.event_time.get()}")
 
 
 if __name__ == "__main__":
