03be9d25f86f11353daf5c77374e7cb6ced2a1af
nikhilcbhat <bhatnikhil314@gmail.com>
2020-10-09 17:31:17 -0400

    Got working calendar widgets

--- gui.py
+++ gui.py
@@ -8,26 +8,27 @@ class GrowOrganicGUI(tk.Tk):
         tk.Tk.__init__(self)
         s = ttk.Style(self)
         s.theme_use('clam')
-        self.calendar_date = None
-        ttk.Button(self, text='Input Date', command=self.getDate).grid(row=0, columnspan=2)
-        ttk.Label(self, text='Event Type:').grid(row=1, column=0)
-        ttk.Entry(self).grid(row=1, column=1)
-        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=2, columnspan=2)
-    
-    def set_date(self):
-        self.calendar_date = self.cal.selection_get()
-
-    def getDate(self):
-        top = tk.Toplevel(self)
-
-        self.cal = Calendar(top,
-                    font="Arial 14", selectmode='day',
-                    cursor="hand1", year=2020, month=10, day=5)
-        self.cal.pack(fill="both", expand=True)
-        ttk.Button(top, text="ok", command=self.set_date).pack()
         
+        self.calendar_date = None
+        
+        self.calendar_widget = Calendar(self,font="Arial 14", selectmode='day',cursor="hand1", year=2020, month=10, day=5)
+        self.calendar_widget.grid(row=0, columnspan=2)
+        
+        ttk.Label(self, text='Select Time:').grid(row=1, column=0)
+        self.event_time = tk.StringVar(self)
+        self.event_time.set("0:00")
+        tk.OptionMenu(self, self.event_time, *[f"{time}:00" for time in range(24)]).grid(row=1, column=1)
+        
+        ttk.Label(self, text='Event Type:').grid(row=2, column=0)
+        self.event_type = tk.StringVar(self)
+        self.event_type.set("AERATE")
+
+        tk.OptionMenu(self, self.event_type, *["AERATE", "WATER"]).grid(row=2, column=1)
+
+        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=3, columnspan=2)
+
     def scheduleEvent(self):
-        print(f"Scheduling event on {self.calendar_date}")
+        print(f"Scheduling {self.event_type.get()} event on {self.calendar_widget.selection_get()} at {self.event_time.get()}")
 
 
 if __name__ == "__main__":
