fb2702eb76a1e89f8f9028569ee3c99ab3b9d39b
nikhilcbhat <bhatnikhil314@gmail.com>
2020-10-09 18:13:35 -0400

    Added frequency, made row more modular

--- gui.py
+++ gui.py
@@ -8,36 +8,52 @@ class GrowOrganicGUI(tk.Tk):
         super().__init__()
         s = ttk.Style(self)
         s.theme_use('clam')
+        self.current_row = 0
 
-        ttk.Label(self, text='Grow Organic!\n', font="Arial 20").grid(row=0, columnspan=2)
+        ttk.Label(self, text='Grow Organic!\n', font="Arial 20").grid(row=self.current_row, columnspan=2)
+        self.current_row += 1
+
         self._add_calendar_widget()
         self._add_event_time_widget()
         self._add_event_type_widget()
-        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=4, columnspan=2)
+        self._add_frequency_widget()
+        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=self.current_row, columnspan=2)
+        self.current_row += 1
 
     def _add_calendar_widget(self):
         self.calendar_date = None
         self.calendar_widget = Calendar(self,font="Arial 14", 
             selectmode='day',cursor="hand1", year=2020, month=10, day=5)
-        self.calendar_widget.grid(row=1, columnspan=2)
+        self.calendar_widget.grid(row=self.current_row, columnspan=2)
+        self.current_row += 1
 
     def _add_event_time_widget(self):
-        ttk.Label(self, text='Select Time:').grid(row=2, column=0)
+        ttk.Label(self, text='Select Time:').grid(row=self.current_row, column=0)
         self.event_time = tk.StringVar(self)
         self.event_time.set("0:00")
-        tk.OptionMenu(self, self.event_time, *[f"{time}:00" for time in range(24)]).grid(row=2, column=1)
+        tk.OptionMenu(self, self.event_time, *[f"{time}:00" for time in range(24)]).grid(row=self.current_row, column=1)
+        self.current_row += 1
 
     def _add_event_type_widget(self):
-        ttk.Label(self, text='Event Type:').grid(row=3, column=0)
+        ttk.Label(self, text='Event Type:').grid(row=self.current_row, column=0)
         self.event_type = tk.StringVar(self)
         self.event_type.set("AERATE")
-        tk.OptionMenu(self, self.event_type, *["AERATE", "WATER"]).grid(row=3, column=1)
+        tk.OptionMenu(self, self.event_type, *["AERATE", "WATER"]).grid(row=self.current_row, column=1)
+        self.current_row += 1
+
+    def _add_frequency_widget(self):
+        ttk.Label(self, text='Frequency:').grid(row=self.current_row, column=0)
+        self.frequency = tk.StringVar(self)
+        self.frequency.set("n/a")
+        tk.OptionMenu(self, self.frequency, *["n/a", "hourly", "daily", "weekly"]).grid(row=self.current_row, column=1)
+        self.current_row += 1
 
     def scheduleEvent(self):
         print(
             f"Scheduling {self.event_type.get()} event on " +
             f"{self.calendar_widget.selection_get()} at {self.event_time.get()}")
-
+        if self.frequency.get() != "n/a":
+            print(f"Will repeat {self.frequency.get()}")
 
 if __name__ == "__main__":
     gui = GrowOrganicGUI()
