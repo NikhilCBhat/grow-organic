48a81bce9966baa8786215f1b7f1bc1a34c0f42f
Nikhil Bhat <bhatnikhil314@gmail.com>
2020-10-09 20:07:02 -0400

    Features/add scheduling buttons (#5)
    * Got working calendar widgets
    
    * Refactored code into functions
    
    * Added title block
    
    * Added frequency, made row more modular

--- gui.py
+++ gui.py
@@ -5,30 +5,55 @@ from tkcalendar import Calendar
 class GrowOrganicGUI(tk.Tk):
 
     def __init__(self):
-        tk.Tk.__init__(self)
+        super().__init__()
         s = ttk.Style(self)
         s.theme_use('clam')
+        self.current_row = 0
+
+        ttk.Label(self, text='Grow Organic!\n', font="Arial 20").grid(row=self.current_row, columnspan=2)
+        self.current_row += 1
+
+        self._add_calendar_widget()
+        self._add_event_time_widget()
+        self._add_event_type_widget()
+        self._add_frequency_widget()
+        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=self.current_row, columnspan=2)
+        self.current_row += 1
+
+    def _add_calendar_widget(self):
         self.calendar_date = None
-        ttk.Button(self, text='Input Date', command=self.getDate).grid(row=0, columnspan=2)
-        ttk.Label(self, text='Event Type:').grid(row=1, column=0)
-        ttk.Entry(self).grid(row=1, column=1)
-        ttk.Button(self, text='Schedule Event!', command=self.scheduleEvent).grid(row=2, columnspan=2)
-    
-    def set_date(self):
-        self.calendar_date = self.cal.selection_get()
+        self.calendar_widget = Calendar(self,font="Arial 14", 
+            selectmode='day',cursor="hand1", year=2020, month=10, day=5)
+        self.calendar_widget.grid(row=self.current_row, columnspan=2)
+        self.current_row += 1
 
-    def getDate(self):
-        top = tk.Toplevel(self)
+    def _add_event_time_widget(self):
+        ttk.Label(self, text='Select Time:').grid(row=self.current_row, column=0)
+        self.event_time = tk.StringVar(self)
+        self.event_time.set("0:00")
+        tk.OptionMenu(self, self.event_time, *[f"{time}:00" for time in range(24)]).grid(row=self.current_row, column=1)
+        self.current_row += 1
 
-        self.cal = Calendar(top,
-                    font="Arial 14", selectmode='day',
-                    cursor="hand1", year=2020, month=10, day=5)
-        self.cal.pack(fill="both", expand=True)
-        ttk.Button(top, text="ok", command=self.set_date).pack()
-        
+    def _add_event_type_widget(self):
+        ttk.Label(self, text='Event Type:').grid(row=self.current_row, column=0)
+        self.event_type = tk.StringVar(self)
+        self.event_type.set("AERATE")
+        tk.OptionMenu(self, self.event_type, *["AERATE", "WATER"]).grid(row=self.current_row, column=1)
+        self.current_row += 1
+
+    def _add_frequency_widget(self):
+        ttk.Label(self, text='Frequency:').grid(row=self.current_row, column=0)
+        self.frequency = tk.StringVar(self)
+        self.frequency.set("n/a")
+        tk.OptionMenu(self, self.frequency, *["n/a", "hourly", "daily", "weekly"]).grid(row=self.current_row, column=1)
+        self.current_row += 1
+
     def scheduleEvent(self):
-        print(f"Scheduling event on {self.calendar_date}")
-
+        print(
+            f"Scheduling {self.event_type.get()} event on " +
+            f"{self.calendar_widget.selection_get()} at {self.event_time.get()}")
+        if self.frequency.get() != "n/a":
+            print(f"Will repeat {self.frequency.get()}")
 
 if __name__ == "__main__":
     gui = GrowOrganicGUI()
