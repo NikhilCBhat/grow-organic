f1b8d0a10468020f2cda91bb3049b840cfd69761
LKField <field.lu@northeastern.edu>
2020-11-11 21:50:38 -0500

    preparing for i2c soil sensors

--- data_collection/moisture.py
+++ data_collection/moisture.py
@@ -1,11 +1,46 @@
 #!/usr/bin/python
-"""Soil Moisture Calibration
-Allows for moisture sensor calibration and output
+"""Soil Moisture Sensor Read
+Allows for moisture sensor
+Source: https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/python-circuitpython-test
 """
+import time
+from board import SCL, SDA
+import busio
+from adafruit_seesaw.seesaw import Seesaw
 
-#TODO: Write these functions
-def calibrate_moisture():
+def moisture_setup(addr):
+    i2c_bus = busio.I2C(SCL, SDA)
+    sensor = Seesaw(i2c_bus, addr)
+    return sensor
+
+def collect_data(sensor):
+    # read soil mosisture
+    soil_moisture = sensor.moisture_read()
+    # read temperature from the temperature sensor
+    soil_temp = sensor.get_temp()
+    return soil_moisture, soil_temp
+
+def print_data(sensor):
+    # read soil mosisture
+    soil_moisture = sensor.moisture_read()
+    # read temperature from the temperature sensor
+    soil_temp = sensor.get_temp()
+    print("Soil temp: " + str(soil_temp) + "  Soil moisture: " + str(soil_moisture))
     return
 
-def data_moisture():
-    return
+def upload_data_to_sensor_table(moisture_data):
+    sensor_names = ["MOISTURE", "SOILTEMP"]
+    for sensor_name, data in zip(sensor_names, moisture_data):
+        upload_data(0, sensor_name, data)
+
+
+#TODO: Figure out how to assign mulitple sensor addresses
+def main()
+    addr=0x36
+    moisture_sensor = moisture_setup(addr)
+    print_data(moisture_sensor)
+#    moisture_data  = collect_data(moisture_sensor)
+#    upload_data_to_sensor_table(moisture_data)
+
+if __name__ == "__main__":
+    main()
--- data_collection/test_sensor_system.py
+++ data_collection/test_sensor_system.py
@@ -7,18 +7,18 @@ from data_collection.temp import main as temp_main
 from data_collection.temp import temp_setup
 from data_collection.light import main as light_main
 from data_collection.light import light_setup
-#from data_collection.adc import *
+#from data_collection.wind import *
 from time import sleep
 
 def main():
     light_sensor = light_setup()
     temp_sensor = temp_setup()
-    # put code for adc here
+    # put code for adc wind sensor here
     while True:
         try:
             light_main(light_sensor)
             temp_main(temp_sensor)
-            # put adc code here
+            # put adc wind sensor code here
             sleep(2)
         except KeyboardInterrupt:
             break
