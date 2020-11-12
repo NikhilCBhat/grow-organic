73858ef4438cec432f9cd274a6ee6bdf0f8bf0df
LKField <field.lu@northeastern.edu>
2020-11-11 21:07:51 -0500

    updates to sensor tests

--- data_collection/temp.py
+++ data_collection/temp.py
@@ -4,20 +4,40 @@ Sets up I2C connection for AHT20 sensor
 Reads in data over I2C
 Source: https://circuitpython.readthedocs.io/projects/ahtx0/en/latest/
 """
+import sys
+sys.path.append('.')
 from time import sleep
 import board
 import busio
 import adafruit_ahtx0
+#from sensor_data.upload_sensor_data import upload_data
 
-def setup_temp():
+def temp_setup():
     # Create library object using our Bus I2C port
     i2c = busio.I2C(board.SCL, board.SDA)
     sensor = adafruit_ahtx0.AHTx0(i2c)
+    return sensor
+
+def collect_data(sensor):
+    temperature = sensor.temperature
+    humidity = sensor.relative_humidity
+    return temperature, humidity
+
+def print_data(sensor):
+    print("\nTemperature: %0.1f C" % sensor.temperature)
+    print("Humidity: %0.1f %%" % sensor.relative_humidity)
     return
 
-def data_temp(delay):
-    while True:
-        print("\nTemperature: %0.1f C" % sensor.temperature)
-        print("Humidity: %0.1f %%" % sensor.relative_humidity)
-        sleep(delay)
-    return sensor.temperature, sensor.relative_humidity
+def upload_data_to_sensor_table(temp_data):
+    sensor_names = ["TEMPERATURE", "HUMIDITY"]
+    for sensor_name, data in zip(sensor_names, temp_data):
+        upload_data(0, sensor_name, data)
+
+def main(temp_sensor):
+    print_data(temp_sensor)
+#    temp_data = collect_data(temp_sensor)
+#    upload_data_to_sensor_table(temp_data)
+#    sleep(10)
+
+if __name__ == "__main__":
+    main()
---
+++ data_collection/test_sensor_system.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""System to test all sensors and print outputs
+"""
+import sys
+sys.path.append('.')
+from data_collection.temp import main as temp_main
+from data_collection.temp import temp_setup
+from data_collection.light import main as light_main
+from data_collection.light import light_setup
+#from data_collection.adc import *
+from time import sleep
+
+def main():
+    light_sensor = light_setup()
+    temp_sensor = temp_setup()
+    # put code for adc here
+    while True:
+        try:
+            light_main(light_sensor)
+            temp_main(temp_sensor)
+            # put adc code here
+            sleep(2)
+        except KeyboardInterrupt:
+            break
+
+if __name__ == "__main__":
+    main()
