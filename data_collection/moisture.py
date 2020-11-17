6b55d5972064337881cfffb055289be970b9a1c1
nikhilcbhat <bhatnikhil314@gmail.com>
2020-11-17 17:46:22 -0500

    Updated mositure sensors

--- data_collection/moisture.py
+++ data_collection/moisture.py
@@ -7,18 +7,26 @@ import time
 from board import SCL, SDA
 import busio
 from adafruit_seesaw.seesaw import Seesaw
+from sensor_data.upload_sensor_data import upload_data
 
-def moisture_setup(addr):
-    i2c_bus = busio.I2C(SCL, SDA)
-    sensor = Seesaw(i2c_bus, addr)
-    return sensor
+def moisture_setup(addrresses):
+    sensors = []
+    for addr in addrresses:
+        i2c_bus = busio.I2C(SCL, SDA)
+        sensor = Seesaw(i2c_bus, addr)
+        sensors.append(sensor)
+    return sensors
 
-def collect_data(sensor):
+def collect_data(sensors):
     # read soil mosisture
-    soil_moisture = sensor.moisture_read()
-    # read temperature from the temperature sensor
-    soil_temp = sensor.get_temp()
-    return soil_moisture, soil_temp
+    moisutures, temps = [], []
+    for sensor in sensors:
+        soil_moisture = sensor.moisture_read()
+        # read temperature from the temperature sensor
+        soil_temp = sensor.get_temp()
+        moisutures.append(soil_moisture)
+        temps.append(soil_temp)
+    return moisutures, temps
 
 def print_data(sensor):
     # read soil mosisture
@@ -30,17 +38,16 @@ def print_data(sensor):
 
 def upload_data_to_sensor_table(moisture_data):
     sensor_names = ["MOISTURE", "SOILTEMP"]
-    for sensor_name, data in zip(sensor_names, moisture_data):
-        upload_data(0, sensor_name, data)
-
+    for i, data in enumerate(moisture_data):
+        for sensor_name, values in zip(sensor_names, data):
+            upload_data(i, sensor_name, values)
 
 #TODO: Figure out how to assign mulitple sensor addresses
-def main()
-    addr=0x36
-    moisture_sensor = moisture_setup(addr)
-    print_data(moisture_sensor)
-#    moisture_data  = collect_data(moisture_sensor)
-#    upload_data_to_sensor_table(moisture_data)
+def main():
+    addrresses = ["0x36"]
+    moisture_sensors = moisture_setup(addrresses)
+    moisture_data  = collect_data(moisture_sensors)
+    upload_data_to_sensor_table(moisture_data)
 
 if __name__ == "__main__":
     main()
