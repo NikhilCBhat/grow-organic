c19b9d343af59977bccec03f77db11eb9cf0a4d9
nikhilcbhat <bhatnikhil314@gmail.com>
2020-11-17 14:09:54 -0500

    Can plot multiple things

--- sensor_data/visualize_sensors.py
+++ sensor_data/visualize_sensors.py
@@ -1,6 +1,6 @@
 import seaborn as sns
 from matplotlib import pyplot as plt
-import numpy as np
+import math
 sns.set_theme()
 import sys
 sys.path.append('.')
@@ -8,13 +8,17 @@ from sensor_data.query_sensor_database import get_data_from_database
 
 def plot_sensor_data():
     data = get_data_from_database()
+    size = math.ceil(math.sqrt(7))
+    f, axes = plt.subplots(size, size)
 
-    for sensor_data in data:
+    for i, sensor_data in enumerate(data):
         sns.lineplot(
             x=sensor_data["timestamps"],
-            y=sensor_data["values"]
+            y=sensor_data["values"],
+            ax=axes[i%size, i//size]
         ).set_title(sensor_data["name"])
-        plt.show()
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     plot_sensor_data()
