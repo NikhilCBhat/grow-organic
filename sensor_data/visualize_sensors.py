981190555fcadee682a2c2cd34113913247827a4
nikhilcbhat <bhatnikhil314@gmail.com>
2020-11-17 14:03:00 -0500

    Updated visualize sensors

--- sensor_data/visualize_sensors.py
+++ sensor_data/visualize_sensors.py
@@ -2,28 +2,12 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import numpy as np
 sns.set_theme()
-
-def get_sensor_data():
-    """
-    TODO
-    1) Connect to DynamoDB Table 
-    2) Get the data for each sensor in the below format
-
-    For now I just have random dummy data
-    """
-    return [
-        {"name":"Temperature", 
-        "timestamps": list(range(31)), 
-        "values": [60]+[70, 75, 80, 85, 70, 77, 79, 82, 80, 77]*3
-        },
-         {"name":"Moisture", 
-        "timestamps": list(range(10)), 
-        "values": np.random.randn(1,10).tolist()[0]
-        }
-    ]
+import sys
+sys.path.append('.')
+from sensor_data.query_sensor_database import get_data_from_database
 
 def plot_sensor_data():
-    data = get_sensor_data()
+    data = get_data_from_database()
 
     for sensor_data in data:
         sns.lineplot(
