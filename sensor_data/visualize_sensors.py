a59258d68dc9ac124fb0efe38541cda629fe0934
Nikhil Bhat <bhatnikhil314@gmail.com>
2020-11-17 14:11:19 -0500

    Features/query and visualize (#21)
    * Fixed querying
    
    * Updated visualize sensors
    
    * Can plot multiple things
    
    * made empty variable for unused var

--- sensor_data/visualize_sensors.py
+++ sensor_data/visualize_sensors.py
@@ -1,36 +1,24 @@
 import seaborn as sns
 from matplotlib import pyplot as plt
-import numpy as np
+import math
 sns.set_theme()
-
-def get_sensor_data():
-    """
-    TODO
-    1) Connect to DynamoDB Table 
-    2) Get the data for each sensor in the below format
-
-    For now I just have random dummy data
-    """
-    return [
-        {"name":"Temperature", 
-        "timestamps": list(range(31)), 
-        "values": [60]+[70, 75, 80, 85, 70, 77, 79, 82, 80, 77]*3
-        },
-         {"name":"Moisture", 
-        "timestamps": list(range(10)), 
-        "values": np.random.randn(1,10).tolist()[0]
-        }
-    ]
+import sys
+sys.path.append('.')
+from sensor_data.query_sensor_database import get_data_from_database
 
 def plot_sensor_data():
-    data = get_sensor_data()
+    data = get_data_from_database()
+    size = math.ceil(math.sqrt(7))
+    _, axes = plt.subplots(size, size)
 
-    for sensor_data in data:
+    for i, sensor_data in enumerate(data):
         sns.lineplot(
             x=sensor_data["timestamps"],
-            y=sensor_data["values"]
+            y=sensor_data["values"],
+            ax=axes[i%size, i//size]
         ).set_title(sensor_data["name"])
-        plt.show()
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     plot_sensor_data()
