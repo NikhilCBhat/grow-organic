64cfe0a97ef7198cebdea4db8a626ce2817e6f27
nikhilcbhat <bhatnikhil314@gmail.com>
2020-12-01 17:43:19 -0500

    Added functions to turn lights & fan on/off

--- data_collection/relay.py
+++ data_collection/relay.py
@@ -6,6 +6,8 @@ Source: https://electronicshobbyists.com/controlling-ac-devices-with-raspberry-p
 
 import RPi.GPIO as GPIO
 from time import sleep
+wind_pin = 27
+light_pin = 17
 
 def setup_relay(relay_pin):
     GPIO.setmode(GPIO.BCM)
@@ -24,6 +26,25 @@ def open_relay(relay_pin, run_time):
     print("Relay open - Outlet off")
     sleep(run_time)
     return
+
+def turn_fan_on():
+    wind_duration = 10
+    setup_relay(wind_pin)
+    close_relay(wind_pin, wind_duration)
+
+def turn_fan_off():
+    setup_relay(wind_pin)
+    open_relay(wind_pin, 1)
+
+def turn_light_on():
+    light_duration = 10
+    setup_relay(light_pin)
+    close_relay(light_pin, light_duration)
+
+def turn_light_off():
+    setup_relay(light_pin)
+    open_relay(light_pin, 1)
+
 
 def main():
     relay_pin1 = 27
